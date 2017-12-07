93409ddc0af9110ebaf15a884a6d961cb1ee903b
Garima <garimachauhan92@yahoo.com>
2017-12-07 14:15:04 +0800

    Changed garimachauhan to garei

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/Samreay/WorkshopExample',
+      url='http://github.com/garie92/WorkshopExample',
       author='Samuel Hinton',
-      author_email='samuelreay@gmail.com',
+      author_email='garie92@yahoo.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
