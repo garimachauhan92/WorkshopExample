12c3c565d170af4effda9be24cd41b21bb131626
Garima <garimachauhan92@yahoo.com>
2017-12-07 14:20:06 +0800

    corrected the mistake

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
-      url='http://github.com/garie92/WorkshopExample',
+      url='http://github.com/garimachauhan92/WorkshopExample',
       author='Samuel Hinton',
-      author_email='garie92@yahoo.com',
+      author_email='garimachauhan92@yahoo.com',
       license='MIT',
       install_requires=['numpy'],
       packages=find_packages(exclude=('tests', 'doc', 'data'))
